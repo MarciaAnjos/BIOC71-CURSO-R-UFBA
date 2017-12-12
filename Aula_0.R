9926f1949f461ccc4e93778340c8c3dc0b74817e
MarciaAnjos <34479284+MarciaAnjos@users.noreply.github.com>
2017-12-12 19:05:59 -0200

    Create Aula_0.R
    "Refazendo o script das atividades preliminares"

---
+++ Aula_0.R
@@ -0,0 +1,60 @@
+1+1
+sum(1,1)
+() {} 
+16+27
+16*27
+16-27
+16 ^27
+2x=17-13
+x=(17-13)/2
+x
+x^2
+sqrt (x)
+log(x)
+?log
+log (x, base=3)
+?linear
+??anova
+sorvete=feijao
+sorvete="feijao"
+sorvete= x
+sorvete= sorvete + 8
+sorvete = 4
+sorvete <- 10
+geladeira= c("sorvete", "alho", "maionese")
+geladeira= c(geladeira, "queijo")
+geladeira = c(3, 5, "chuchu")
+numeros = c(1,2,3,3)
+numeros 1:10
+1:10
+seq(from = 1, to = 100, by=3)
+seq(from = 1, to 10, by=0.5)
+seq(from=1, to=10, by=0.5)
+rep("coxinhas", 5)
+rep(70, 5)
+length(numeros)
+numeros[3]
+view(dados)
+View(dados)
+vetor3=letters
+vetor4=26:1
+nomesColunas= c("letras", "numeros")
+dadosnomeados=data.frame(vetor3, y=vetor4)
+colnames(dadosnomeados) = nomesColunas
+head(dadosnomeados)
+View(dados)
+vetor1=LETTERS
+vetor2=1:26
+nomescolunas= c("LETTERS", "Numeros")
+dadosnomeados=data.frame(vetor1, vetor2)
+head(dadosnomeados)
+dadosnomeados[1,2]
+dadosnomeados[1,3]
+dadosnomeados$Letras
+adodsnomeados$vetor1[5]
+idade = c(3.0, 4.0, 5.0, 6.0,8.0, 9.0, 10.0, 11.0,12.0, 14.0, 15.0, 16.0, 17.0)
+idade[c(5, 4, 1, 10,12)]
+idade >6
+idade >=6
+idade[idade>6]
+idade[idade<3]
